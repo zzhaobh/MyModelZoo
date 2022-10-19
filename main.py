b3cb79f743881d53c1a7c4496d0162c7ca93c565
zzhaobh <zzhaobh@connect.ust.hk>
2022-10-19 23:30:29 +0800

    dataloader可用

--- main.py
+++ main.py
@@ -5,5 +5,13 @@ play model begin！
 print("hellow model zoo！")
 
 from DowloadData import DownloadTextData
+from torch.utils.data import DataLoader
 
 dataset = DownloadTextData.load_helper(dataset_name="rotten_tomatoes", split="train", download=True, download_dir="./download_datasets")
+
+dataset.set_format(type="torch", columns=["text", "label"])
+dataloader = DataLoader(dataset, batch_size=4)
+
+for i in dataloader:
+    print(i)
+    break
